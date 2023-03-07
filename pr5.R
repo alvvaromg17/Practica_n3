76baac6a2122a080bbe9d3f5e9381a0b931e8517
alvvaromg17 <alvvaromg17@gmail.com>
2023-03-07 12:00:25 +0100

    Update pr5.R

--- pr5.R
+++ pr5.R
@@ -1,2 +1,3 @@
-c aaaaaaaaaaaaaaaaaaaaa
-aa
+numArtefactos = c(17, 54, 10, 34, 90, 33, 49, 82, 12, 23, 56, 78, 44, 102, 10, 53, 4, 28, 37, 95)
+as.integer (numArtefactos_int)
+is.integer (numArtefactos_int)

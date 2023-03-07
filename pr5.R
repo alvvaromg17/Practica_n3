3128f4eff660bdfa4ae59f54cff6215ed7772ce9
alvvaromg17 <alvvaromg17@gmail.com>
2023-03-07 12:09:03 +0100

    Update pr5.R

--- pr5.R
+++ pr5.R
@@ -1,3 +1,10 @@
+numArtefactos = c (as.integer (17, 54, 10, 34, 90, 33, 49, 82, 12, 23, 56, 78, 44, 102, 10, 53, 4, 28, 37, 95))
 numArtefactos = c(17, 54, 10, 34, 90, 33, 49, 82, 12, 23, 56, 78, 44, 102, 10, 53, 4, 28, 37, 95)
-as.integer (numArtefactos_int)
+numArtefactos_int = as.integer (numArtefactos_int)
 is.integer (numArtefactos_int)
+is.vector(numArtefactos_int)
+
+mean (numArtefactos_int)
+median (numArtefactos_int)
+
+moda =
